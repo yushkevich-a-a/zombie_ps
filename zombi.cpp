ab38b17e18b904cbace8a62045cce6882436fcb3
Anton <yushkevich.a.a@yandex.ru>
2024-05-22 18:40:11 +0000

    zombi

---
+++ zombi.cpp
@@ -0,0 +1,20 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t child_pid;
+    child_pid = fork();
+
+    if (child_pid > 0) 
+    {
+        sleep(60);
+    }
+    else
+    {
+        exit(0);
+    }
+    return 0;
+
+}
